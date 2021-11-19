17c6b0269f15e90c6fa635cb9a6c311853b5a4b3
daehyun <rkdeown10@naver.com>
2021-11-19 16:50:32 +0900

    third commit

--- run.py
+++ run.py
@@ -1,95 +1,117 @@
 import sys
-
 from PyQt5 import uic
+from PyQt5.QtCore import *
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from time import sleep
 from datetime import datetime
+from time import sleep
 
 form_class = uic.loadUiType("traffic_ui.ui")[0]
 
 
 class Thread1(QThread):
-    signal = pyqtSignal(str, int)
+    signal = pyqtSignal(str)
 
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
 
     def run(self):
-        signals = ['RED', 'YELLOW', 'GREEN', 'LEFT']
-        cnt = 0
+        signals = ['GREEN', 'YELLOW', 'LEFT', 'YELLOW', 'RED']
         while True:
             for signal in signals:
-                self.signal.emit(signal, cnt)
-                cnt = cnt + 1
+
+                self.signal.emit(signal)
                 if signal == 'RED':
                     self.parent.RED.setStyleSheet("background-color: rgb(255, 0, 0);\n"
-                                                  "border-radius: 45px;\n"
-                                                  "min-height: 10px;\n"
-                                                  "min-width: 10px;")
+                                                  "border-radius: 50px;\n"
+                                                  "min-height: 100px;\n"
+                                                  "min-width: 100px;")
                     sleep(1)
-
                     self.parent.RED.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-                                                  "border-radius: 45px;\n"
-                                                  "min-height: 10px;\n"
-                                                  "min-width: 10px;")
-                if signal == 'YELLOW':
+                                                  "border-radius: 50px;\n"
+                                                  "min-height: 100px;\n"
+                                                  "min-width: 100px;")
+                elif signal == 'YELLOW':
                     self.parent.YELLOW.setStyleSheet("background-color: rgb(255, 255, 0);\n"
-                                                     "border-radius: 45px;\n"
-                                                     "min-height: 10px;\n"
-                                                     "min-width: 10px;")
+                                                     "border-radius: 50px;\n"
+                                                     "min-height: 100px;\n"
+                                                     "min-width: 100px;")
                     sleep(1)
                     self.parent.YELLOW.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-                                                     "border-radius: 45px;\n"
-                                                     "min-height: 10px;\n"
-                                                     "min-width: 10px;")
-                if signal == 'GREEN':
+                                                     "border-radius: 50px;\n"
+                                                     "min-height: 100px;\n"
+                                                     "min-width: 100px;")
+                elif signal == 'GREEN':
                     self.parent.GREEN.setStyleSheet("background-color: rgb(0, 255, 0);\n"
-                                                    "border-radius: 45px;\n"
-                                                    "min-height: 10px;\n"
-                                                    "min-width: 10px;")
+                                                    "border-radius: 50px;\n"
+                                                    "min-height: 100px;\n"
+                                                    "min-width: 100px;")
                     sleep(1)
                     self.parent.GREEN.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-                                                    "border-radius: 45px;\n"
-                                                    "min-height: 10px;\n"
-                                                    "min-width: 10px;")
-                if signal == 'LEFT':
+                                                    "border-radius: 50px;\n"
+                                                    "min-height: 100px;\n"
+                                                    "min-width: 100px;")
+                elif signal == 'LEFT':
                     self.parent.LEFT.setStyleSheet("color: rgb(0, 255, 0);\n"
-                                                   "border-radius: 45px;\n"
-                                                   "min-height: 10px;\n"
-                                                   "min-width: 10px;")
+                                                   "background-color: rgb(0, 0, 0);\n"
+                                                   "line-height: 100px;\n"
+                                                   "border-radius: 50px;\n"
+                                                   "border: 3px solid rgb(0, 255, 0);\n"
+                                                   "min-height: 100px;\n"
+                                                   "min-width: 100px;")
                     sleep(1)
                     self.parent.LEFT.setStyleSheet("color: rgb(0, 0, 0);\n"
-                                                   "border-radius: 45px;\n"
-                                                   "min-height: 10px;\n"
-                                                   "min-width: 10px;")
+                                                   "background-color: rgb(0, 0, 0);\n"
+                                                   "line-height: 100px;\n"
+                                                   "border-radius: 50px;\n"
+                                                   "min-height: 100px;\n"
+                                                   "min-width: 100px;")
 
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("흙살림")
+        self.setWindowTitle("신호등")
         self.setFont(QFont('나눔스퀘어_ac', 12))
+        # self.setFixedSize(self.size())
         h1 = Thread1(self)
         h1.signal.connect(self.change_traffic_light)
         h1.start()
+        self.pushButton.clicked.connect(self.button1Function)
+        self.pushButton_2.clicked.connect(self.button2Function)
 
-    @pyqtSlot(str, int)
-    def change_traffic_light(self, signal, cnt):
+    def button1Function(self):
+        print("삭제버튼을 눌렀습니다")
+
+    def button2Function(self):
+        print("저장버튼을 눌렀습니다")
+
+    @pyqtSlot(str)
+    def change_traffic_light(self, signal):
         if signal == 'RED':
-            print(signal + "시그널을 Slot으로 받았습니다")
+            # print(signal + "시그널을 Slot으로 받았습니다")
+            pass
         elif signal == 'YELLOW':
-            print(signal + '시그널을 Slot으로 받았습니다')
+            # print(signal + '시그널을 Slot으로 받았습니다')
+            pass
         elif signal == 'LEFT':
-            print(signal + '시그널을 Slot으로 받았습니다')
+            # print(signal + '시그널을 Slot으로 받았습니다')
+            pass
         elif signal == 'GREEN':
-            print(signal + '시그널을 Slot으로 받았습니다')
-        print("시간 : " + datetime.today().strftime("%Y/%m/%d %H:%M:%S"))
-        print("신호등 : " + signal)
-        print(cnt)
+            # print(signal + '시그널을 Slot으로 받았습니다')
+            pass
+        rowPosition = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(rowPosition)
+
+        self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(datetime.today().strftime("%Y/%m/%d\n%H:%M:%S")))
+        self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem(signal))
+        self.tableWidget.resizeRowsToContents()
+        # self.tableWidget.resizeColumnToContents(0)
+        # self.tableWidget.resizeColumnToContents(1)
+        # self.tableWidget.resizeColumnsToContents()
+        self.tableWidget.scrollToBottom()
 
 
 if __name__ == "__main__":

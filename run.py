b7c8e0c3a4ee9e814168f9f4841fa879d45b6de4
daehyun <rkdeown10@naver.com>
2021-11-22 15:35:33 +0900

    commit

--- run.py
+++ run.py
@@ -1,72 +1,48 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QColor
 from PyQt5.QtWidgets import *
 from datetime import datetime
 from time import sleep
+import csv
+import os
 
+root = 'C:/traffic_data/'
+file_name = datetime.today().strftime("%Y-%m-%d %H-%M-%S")
+extension = '.csv'
+path = root + file_name + extension
 form_class = uic.loadUiType("traffic_ui.ui")[0]
+stop_check = True
 
 
 class Thread1(QThread):
+    global stop_check
     signal = pyqtSignal(str)
 
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
+        self.check = stop_check
 
     def run(self):
         signals = ['GREEN', 'YELLOW', 'LEFT', 'YELLOW', 'RED']
         while True:
             for signal in signals:
+                sleep(1)
+                if self.check:
+                    self.signal.emit(signal)
 
-                self.signal.emit(signal)
-                if signal == 'RED':
-                    self.parent.RED.setStyleSheet("background-color: rgb(255, 0, 0);\n"
-                                                  "border-radius: 50px;\n"
-                                                  "min-height: 100px;\n"
-                                                  "min-width: 100px;")
-                    sleep(1)
-                    self.parent.RED.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-                                                  "border-radius: 50px;\n"
-                                                  "min-height: 100px;\n"
-                                                  "min-width: 100px;")
-                elif signal == 'YELLOW':
-                    self.parent.YELLOW.setStyleSheet("background-color: rgb(255, 255, 0);\n"
-                                                     "border-radius: 50px;\n"
-                                                     "min-height: 100px;\n"
-                                                     "min-width: 100px;")
-                    sleep(1)
-                    self.parent.YELLOW.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-                                                     "border-radius: 50px;\n"
-                                                     "min-height: 100px;\n"
-                                                     "min-width: 100px;")
-                elif signal == 'GREEN':
-                    self.parent.GREEN.setStyleSheet("background-color: rgb(0, 255, 0);\n"
-                                                    "border-radius: 50px;\n"
-                                                    "min-height: 100px;\n"
-                                                    "min-width: 100px;")
-                    sleep(1)
-                    self.parent.GREEN.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-                                                    "border-radius: 50px;\n"
-                                                    "min-height: 100px;\n"
-                                                    "min-width: 100px;")
-                elif signal == 'LEFT':
-                    self.parent.LEFT.setStyleSheet("color: rgb(0, 255, 0);\n"
-                                                   "background-color: rgb(0, 0, 0);\n"
-                                                   "line-height: 100px;\n"
-                                                   "border-radius: 50px;\n"
-                                                   "border: 3px solid rgb(0, 255, 0);\n"
-                                                   "min-height: 100px;\n"
-                                                   "min-width: 100px;")
-                    sleep(1)
-                    self.parent.LEFT.setStyleSheet("color: rgb(0, 0, 0);\n"
-                                                   "background-color: rgb(0, 0, 0);\n"
-                                                   "line-height: 100px;\n"
-                                                   "border-radius: 50px;\n"
-                                                   "min-height: 100px;\n"
-                                                   "min-width: 100px;")
+    def stop(self):
+        if self.check:
+            self.check = False
+            self.parent.pushButton.setText("실행")
+            self.parent.pushButton.repaint()
+        else:
+            self.check = True
+            self.parent.pushButton.setText("멈춤")
+            self.parent.pushButton.repaint()
+        print("멈춤버튼을 눌렀습니다")
 
 
 class WindowClass(QMainWindow, form_class):
@@ -75,42 +51,87 @@ class WindowClass(QMainWindow, form_class):
         self.setupUi(self)
         self.setWindowTitle("신호등")
         self.setFont(QFont('나눔스퀘어_ac', 12))
-        # self.setFixedSize(self.size())
-        h1 = Thread1(self)
-        h1.signal.connect(self.change_traffic_light)
-        h1.start()
+        self.h1 = Thread1(self)
+        self.h1.signal.connect(self.change_traffic_light)
+        self.h1.start()
         self.pushButton.clicked.connect(self.button1Function)
         self.pushButton_2.clicked.connect(self.button2Function)
+        self.pushButton_3.clicked.connect(self.button3Function)
 
     def button1Function(self):
-        print("삭제버튼을 눌렀습니다")
+        self.h1.stop()
 
     def button2Function(self):
+        self.h1.stop()
+        data = []
+
+        row = self.tableWidget.rowCount()
+        for row in range(0, row):
+            data.extend([[self.tableWidget.item(row, 0).text(), self.tableWidget.item(row, 1).text()]])
+
+        f = open(path, "w", newline="")
+        wr = csv.writer(f)
+        for row in data:
+            wr.writerow(row)
+        f.close()
+
         print("저장버튼을 눌렀습니다")
+        self.h1.stop()
+        self.tableWidget.setRowCount(0)
+
+    def button3Function(self):
+        print("삭제버튼을 눌렀습니다")
 
     @pyqtSlot(str)
-    def change_traffic_light(self, signal):
-        if signal == 'RED':
-            # print(signal + "시그널을 Slot으로 받았습니다")
-            pass
-        elif signal == 'YELLOW':
-            # print(signal + '시그널을 Slot으로 받았습니다')
-            pass
-        elif signal == 'LEFT':
-            # print(signal + '시그널을 Slot으로 받았습니다')
-            pass
-        elif signal == 'GREEN':
-            # print(signal + '시그널을 Slot으로 받았습니다')
-            pass
+    def change_traffic_light(self, input):
+
+        signal_labels = {'RED': self.RED, 'LEFT': self.LEFT, 'YELLOW': self.YELLOW, 'GREEN': self.GREEN}
+        signal_lst = [self.RED, self.LEFT, self.YELLOW, self.GREEN]
+        for signal in signal_lst:
+            signal.setStyleSheet("color: rgb(0, 0, 0);\n"
+                                 "background-color: rgb(0, 0, 0);\n"
+                                 "line-height: 100px;\n"
+                                 "border-radius: 50px;\n"
+                                 "min-height: 100px;\n"
+                                 "min-width: 100px;")
+            if input.__eq__('RED'):
+                signal_labels[input].setStyleSheet("background-color: rgb(255, 0, 0);\n"
+                                                   "border-radius: 50px;\n"
+                                                   "min-height: 100px;\n"
+                                                   "min-width: 100px;")
+            if input.__eq__('YELLOW'):
+                signal_labels[input].setStyleSheet("background-color: rgb(255, 255, 0);\n"
+                                                   "border-radius: 50px;\n"
+                                                   "min-height: 100px;\n"
+                                                   "min-width: 100px;")
+
+            if input.__eq__('LEFT'):
+                signal_labels[input].setStyleSheet("color: rgb(0, 255, 0);\n"
+                                                   "background-color: rgb(0, 0, 0);\n"
+                                                   "line-height: 100px;\n"
+                                                   "border-radius: 50px;\n"
+                                                   "border: 3px solid rgb(0, 255, 0);\n"
+                                                   "min-height: 100px;\n"
+                                                   "min-width: 100px;")
+                # # 좌회전 + 빨강
+                # self.RED.setStyleSheet("background-color: rgb(255, 0, 0);\n"
+                #                                    "border-radius: 50px;\n"
+                #                                    "min-height: 100px;\n"
+                #                                    "min-width: 100px;")
+
+            if input.__eq__('GREEN'):
+                signal_labels[input].setStyleSheet("background-color: rgb(0, 255, 0);\n"
+                                                   "border-radius: 50px;\n"
+                                                   "min-height: 100px;\n"
+                                                   "min-width: 100px;")
+
         rowPosition = self.tableWidget.rowCount()
         self.tableWidget.insertRow(rowPosition)
-
-        self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(datetime.today().strftime("%Y/%m/%d\n%H:%M:%S")))
-        self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem(signal))
-        self.tableWidget.resizeRowsToContents()
-        # self.tableWidget.resizeColumnToContents(0)
-        # self.tableWidget.resizeColumnToContents(1)
-        # self.tableWidget.resizeColumnsToContents()
+        lists = [QTableWidgetItem(input), QTableWidgetItem(datetime.today().strftime("%Y/%m/%d %H:%M:%S"))]
+        for index, row in enumerate(lists):
+            self.tableWidget.setItem(rowPosition, index, row)
+            self.tableWidget.horizontalHeader().setSectionResizeMode(index, QHeaderView.Stretch)
+            self.tableWidget.item(rowPosition, index).setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
         self.tableWidget.scrollToBottom()
 
 

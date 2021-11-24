369fb908caec2fce45125d0b76664578b1702d31
daehyun <rkdeown10@naver.com>
2021-11-24 14:11:08 +0900

    commit

--- run.py
+++ run.py
@@ -1,41 +1,78 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem, QColor, QIcon
 from PyQt5.QtWidgets import *
 from datetime import datetime
 from time import sleep
 import csv
 import os
+from glob import glob
+from pathlib import Path
+from PyQt5.QtGui import QIcon
+
+
+class StandardItem(QStandardItem):
+    def __init__(self, txt='', font_size=12, set_bold=False, color=QColor(0, 0, 0)):
+        super().__init__()
+
+        fnt = QFont('Open Sans', font_size)
+        fnt.setBold(set_bold)
+
+        self.setEditable(False)
+        self.setForeground(color)
+        self.setFont(fnt)
+        self.setText(txt)
+
+
+def load_project_structure(root, tree):
+    import os
+    from PyQt5.QtWidgets import QTreeWidgetItem
+    for element in os.listdir(root):
+        path_info = root + "/" + element
+        parent_itm = QTreeWidgetItem(tree, [os.path.basename(element)])
+        if os.path.isdir(path_info):
+            load_project_structure(path_info, parent_itm)
+            parent_itm.setIcon(0, QIcon('image/folder.png'))
+        else:
+            parent_itm.setIcon(0, QIcon('image/file.png'))
+
 
 form_class = uic.loadUiType("traffic_ui.ui")[0]
-stop_check = True
+stop_check = False
 
 
 class Thread1(QThread):
-    global stop_check
     signal = pyqtSignal(str)
 
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.check = stop_check
+        self.date = []
+        f = open('traffic.csv', 'r', encoding='utf-8')
+        rdr = csv.reader(f)
+
+        for line in rdr:
+            self.date.extend([line[0]])
+            # self.date.extend([line[1]])
+        f.close()
 
     def run(self):
-        signals = ['GREEN', 'YELLOW', 'LEFT', 'YELLOW', 'RED']
+        global stop_check
         while True:
-            for signal in signals:
+            for signal in self.date:
                 sleep(1)
-                if self.check:
+                if stop_check:
                     self.signal.emit(signal)
 
     def stop(self):
-        if self.check:
-            self.check = False
+        global stop_check
+        if stop_check:
+            stop_check = False
             self.parent.pushButton.setText("실행")
             self.parent.pushButton.repaint()
         else:
-            self.check = True
+            stop_check = True
             self.parent.pushButton.setText("멈춤")
             self.parent.pushButton.repaint()
         print("멈춤버튼을 눌렀습니다")
@@ -53,12 +90,18 @@ class WindowClass(QMainWindow, form_class):
         self.pushButton.clicked.connect(self.button1Function)
         self.pushButton_2.clicked.connect(self.button2Function)
         self.pushButton_3.clicked.connect(self.button3Function)
+        # load_project_structure("C:/traffic_data/", self.treeWidget)
+        self.set_tree_view()
+        self.treeView.setIndentation(0)
+        self.treeModel2 = QStandardItemModel()
+        self.rootNode2 = self.treeModel2.invisibleRootItem()
 
     def button1Function(self):
         self.h1.stop()
 
     def button2Function(self):
-        self.h1.stop()
+        global stop_check
+        stop_check = False
         data = []
         # root = 'C:/traffic_data/'
         root2 = 'C:/traffic_data/' + datetime.today().strftime("%Y-%m-%d") + "/"
@@ -71,7 +114,7 @@ class WindowClass(QMainWindow, form_class):
                 os.makedirs(root2)
         except OSError:
             print('이미 파일이 있습니다' + root2)
-
+        self.tableWidget.setSelectionMode(QAbstractItemView.NoSelection)
         row = self.tableWidget.rowCount()
         for row in range(0, row):
             data.extend([[self.tableWidget.item(row, 0).text(), self.tableWidget.item(row, 1).text()]])
@@ -79,19 +122,56 @@ class WindowClass(QMainWindow, form_class):
         if len(data) != 0:
             f = open(path, "w", newline="")
             wr = csv.writer(f)
+            wr.writerow(["신호", "시간"])
             for row in data:
                 wr.writerow(row)
             f.close()
-
+        # load_project_structure("C:/traffic_data/", self.treeWidget)
+        self.set_tree_view()
         print("저장버튼을 눌렀습니다")
         self.tableWidget.setRowCount(0)
-        self.h1.stop()
+        # stop_check = True
+        self.pushButton.setText("실행")
+        self.pushButton.repaint()
+
+
+    def set_tree_view(self):
+        self.treeView.setHeaderHidden(True)
+        self.treeView.setContextMenuPolicy(Qt.CustomContextMenu)
+        # self.treeWidget.customContextMenuRequested.connect(self.openMenu)
+
+        treeModel = QStandardItemModel()
+        rootNode = treeModel.invisibleRootItem()
+        paths = glob('C:/traffic_data/*')
+
+        for path in paths:
+            folder = StandardItem(Path(path).stem, 16, set_bold=True)
+            files = glob(path + '/*')
+            for file in files:
+                imgs = StandardItem(Path(file).stem, 14)
+                folder.appendRow(imgs)
+
+            rootNode.appendRow(folder)
+
+        self.treeView.setModel(treeModel)
+        self.treeView.collapseAll()
+        # self.treeView.doubleClicked.connect(self.new_window)
 
     def button3Function(self):
+
         print("삭제버튼을 눌렀습니다")
 
     @pyqtSlot(str)
     def change_traffic_light(self, inputsignal):
+
+        rowPosition = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(rowPosition)
+        lists = [QTableWidgetItem(inputsignal), QTableWidgetItem(datetime.today().strftime("%Y/%m/%d %H:%M:%S"))]
+        for index, row in enumerate(lists):
+            self.tableWidget.setItem(rowPosition, index, row)
+            self.tableWidget.horizontalHeader().setSectionResizeMode(index, QHeaderView.Stretch)
+            self.tableWidget.item(rowPosition, index).setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        self.tableWidget.scrollToBottom()
 
         signal_labels = {'RED': self.RED, 'LEFT': self.LEFT, 'YELLOW': self.YELLOW, 'GREEN': self.GREEN}
         signal_lst = [self.RED, self.LEFT, self.YELLOW, self.GREEN]
@@ -128,15 +208,6 @@ class WindowClass(QMainWindow, form_class):
                                                          "border-radius: 50px;\n"
                                                          "min-height: 100px;\n"
                                                          "min-width: 100px;")
-
-        rowPosition = self.tableWidget.rowCount()
-        self.tableWidget.insertRow(rowPosition)
-        lists = [QTableWidgetItem(inputsignal), QTableWidgetItem(datetime.today().strftime("%Y/%m/%d %H:%M:%S"))]
-        for index, row in enumerate(lists):
-            self.tableWidget.setItem(rowPosition, index, row)
-            self.tableWidget.horizontalHeader().setSectionResizeMode(index, QHeaderView.Stretch)
-            self.tableWidget.item(rowPosition, index).setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.tableWidget.scrollToBottom()
 
 
 if __name__ == "__main__":
